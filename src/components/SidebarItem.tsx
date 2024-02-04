71bde4678faac77961aba0ee30797d975c379443
WavyBoyJodii <najeecasey@gmail.com>
2024-02-03 21:33:53 -0500

    create sidebar item component

---
+++ src/components/SidebarItem.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import { LucideIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+interface SidebarItemProps {
+  icon: LucideIcon;
+  label: string;
+  active?: boolean;
+  href: string;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  icon: Icon,
+  label,
+  active,
+  href,
+}) => {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        `
+          flex 
+          flex-row 
+          h-auto 
+          items-center 
+          w-full 
+          gap-x-4 
+          text-md 
+          font-medium
+          cursor-pointer
+          hover:text-white
+          transition
+          text-neutral-400
+          py-1`,
+        { 'text-white': active }
+      )}
+    >
+      <Icon size={26} />
+      <p className="truncate w-100">{label}</p>
+    </Link>
+  );
+};
+
+export default SidebarItem;
