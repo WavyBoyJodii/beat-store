e01874bf8ef3e07a20d67a8bce3aa2e3db9eba49
WavyBoyJodii <najeecasey@gmail.com>
2024-02-03 21:33:12 -0500

    create Sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { Home, Search } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+
+import Box from './Box';
+import { Beat } from '@/types';
+import usePlayer from '@/hooks/usePlayer';
+import SidebarItem from './SidebarItem';
+import Library from './Library';
+
+interface SidebarProps {
+  children: React.ReactNode;
+  //   beats: Beat[];
+}
+
+// TODO: add beats back to props to send via Layout to use for recently played???? probably not
+
+const Sidebar = ({ children }: SidebarProps) => {
+  const pathname = usePathname();
+  const player = usePlayer();
+
+  const routes = useMemo(
+    () => [
+      {
+        icon: Home,
+        label: 'Home',
+        active: pathname !== '/search',
+        href: '/',
+      },
+      {
+        icon: Search,
+        label: 'Search',
+        href: '/search',
+        active: pathname === '/search',
+      },
+    ],
+    [pathname]
+  );
+
+  return (
+    <div
+      className={cn('flex h-full', { 'h-[calc(100%-80px)]': player.activeId })}
+    >
+      <div className=" hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p2">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        {/* TODO: Create useRecentBeats hook to send recentlyPlayed to Library component */}
+        <Box className="overflow-y-auto h-full">
+          <Library recentlyPlayed="nada" />
+        </Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
