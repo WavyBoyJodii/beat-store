685d8aae596aa215a0bb64d776496f454a08bfda
WavyBoyJodii <najeecasey@gmail.com>
2024-02-04 13:16:56 -0500

    create PlayButton component

---
+++ src/components/PlayButton.tsx
@@ -0,0 +1,33 @@
+import { PlaySquare } from 'lucide-react';
+
+interface PlayButtonProps {
+  onClick: () => void;
+}
+
+const PlayButton: React.FC<PlayButtonProps> = ({ onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="
+          transition 
+          opacity-0 
+          rounded-full 
+          flex 
+          items-center 
+          justify-center 
+          bg-green-500 
+          p-4 
+          drop-shadow-md 
+          translate
+          translate-y-1/4
+          group-hover:opacity-100 
+          group-hover:translate-y-0
+          hover:scale-110
+        "
+    >
+      <PlaySquare className="text-black" />
+    </button>
+  );
+};
+
+export default PlayButton;
