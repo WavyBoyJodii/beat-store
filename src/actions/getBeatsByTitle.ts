841cdd9dbc6b482cd02a2abfb1c60e2504e92ad6
WavyBoyJodii <najeecasey@gmail.com>
2024-02-05 12:16:24 -0500

    create getBeatsByTitle to use in Search Function

---
+++ src/actions/getBeatsByTitle.ts
@@ -0,0 +1,31 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies, headers } from 'next/headers';
+
+import { Beat } from '@/types';
+
+import getBeats from './getBeats';
+
+const getBeatsByTitle = async (title: string): Promise<Beat[]> => {
+  const supabase = createServerComponentClient({
+    cookies: cookies,
+  });
+
+  if (!title) {
+    const allBeats = await getBeats();
+    return allBeats;
+  }
+
+  const { data, error } = await supabase
+    .from('beats')
+    .select('*')
+    .ilike('title', `%${title}%`)
+    .order('created_at', { ascending: false });
+
+  if (error) {
+    console.log(error.message);
+  }
+
+  return (data as any) || [];
+};
+
+export default getBeatsByTitle;
