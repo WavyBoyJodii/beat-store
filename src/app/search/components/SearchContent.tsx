253d3fee69c6174bf386fcbc4cb2f30c4f073011
WavyBoyJodii <najeecasey@gmail.com>
2024-02-05 12:46:12 -0500

    create searchContent component to display search results

---
+++ src/app/search/components/SearchContent.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Beat } from '@/types';
+import MediaItem from '@/components/MediaItem';
+import LikeButton from '@/components/LikeButton';
+import useOnPlay from '@/hooks/useOnPlay';
+
+interface SearchContentProps {
+  beats: Beat[];
+}
+
+const SearchContent: React.FC<SearchContentProps> = ({ beats }) => {
+  const onPlay = useOnPlay(beats);
+
+  if (beats.length === 0) {
+    return (
+      <div
+        className="
+          flex 
+          flex-col 
+          gap-y-2 
+          w-full 
+          px-6 
+          text-neutral-400
+        "
+      >
+        No songs found.
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+      {beats.map((beat: Beat) => (
+        <div key={beat.id} className="flex items-center gap-x-4 w-full">
+          <div className="flex-1">
+            <MediaItem onClick={(id: string) => onPlay(id)} data={beat} />
+          </div>
+          <LikeButton beatId={beat.id} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SearchContent;
