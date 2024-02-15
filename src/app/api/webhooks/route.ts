61f3bee4bcc5321a37f379a4c7fffb0ef166c199
WavyBoyJodii <najeecasey@gmail.com>
2024-02-15 15:50:38 -0500

    finish webhook api for stripe to handle checkout session

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -6,6 +6,7 @@ import { stripe } from '@/lib/stripe';
 import {
   upsertProductRecord,
   upsertPriceRecord,
+  addToPurchasedBeats,
 } from '@/lib/supabaseAdmin';
 
 const relevantEvents = new Set([
@@ -14,36 +15,63 @@ const relevantEvents = new Set([
   'price.created',
   'price.updated',
   'checkout.session.completed',
-  'payment_intent.succeeded'
+  'payment_intent.succeeded',
 ]);
 
-export async function POST(
-    request: Request
-  ) {
-      const body = await request.text()
-      const sig = headers().get('Stripe-Signature');
-  
-      const webhookSecret =
-        process.env.STRIPE_WEBHOOK_SECRET_LIVE ??
-        process.env.STRIPE_WEBHOOK_SECRET;
-      let event: Stripe.Event;
-  
-      try {
-        if (!sig || !webhookSecret) return;
-        event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
-      } catch (err: any) {
-        console.log(`❌ Error message: ${err.message}`);
-        return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
+export async function POST(request: Request) {
+  const body = await request.text();
+  const sig = headers().get('Stripe-Signature');
+
+  const webhookSecret =
+    process.env.STRIPE_WEBHOOK_SECRET_LIVE ?? process.env.STRIPE_WEBHOOK_SECRET;
+  let event: Stripe.Event;
+
+  try {
+    if (!sig || !webhookSecret) return;
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+  } catch (err: any) {
+    console.log(`❌ Error message: ${err.message}`);
+    return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
+  }
+
+  if (relevantEvents.has(event.type)) {
+    try {
+      switch (event.type) {
+        case 'product.created':
+        case 'product.updated':
+          await upsertProductRecord(event.data.object as Stripe.Product);
+          break;
+        case 'price.created':
+        case 'price.updated':
+          await upsertPriceRecord(event.data.object as Stripe.Price);
+          break;
+        case 'checkout.session.completed':
+          const checkoutSession = event.data.object as Stripe.Checkout.Session;
+          if (checkoutSession.payment_status === 'paid') {
+            const customerId = checkoutSession.customer as string;
+            console.log(`logging customer Id from stripe ${customerId}`);
+            const lineItems = await stripe.checkout.sessions.listLineItems(
+              checkoutSession.id,
+              { limit: 100 }
+            );
+            lineItems.data.forEach(async (item) => {
+              if (item.price?.id) {
+                await addToPurchasedBeats(customerId, item.price.id);
+              }
+            });
+          }
+          break;
+        default:
+          throw new Error('Unhandled relevant event!');
       }
-  
-    if (relevantEvents.has(event.type)) {
-      try {
-        switch (event.type) {
-          case 'product.created':
-          case 'product.updated':
-            await upsertProductRecord(event.data.object as Stripe.Product);
-            break;
-          case 'price.created':
-          case 'price.updated':
-            await upsertPriceRecord(event.data.object as Stripe.Price);
-            break;
+    } catch (error) {
+      console.log(error);
+      return new NextResponse(
+        'Webhook error: "Webhook handler failed. View logs."',
+        { status: 400 }
+      );
+    }
+  }
+
+  return NextResponse.json({ received: true }, { status: 200 });
+}
