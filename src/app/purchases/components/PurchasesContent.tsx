bddd08f64e89c5a650550893a8f9ac58afea5790
WavyBoyJodii <najeecasey@gmail.com>
2024-02-16 01:09:39 -0500

    create purchases route

---
+++ src/app/purchases/components/PurchasesContent.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { Beat } from '@/types';
+import { useUser } from '@/hooks/useUser';
+import MediaItem from '@/components/MediaItem';
+import LikeButton from '@/components/LikeButton';
+import useOnPlay from '@/hooks/useOnPlay';
+import { DownloadButton } from '@/components/DownloadButton';
+import useAuthModal from '@/hooks/useAuthModal';
+
+interface PurchasesContentProps {
+  beats: Beat[];
+}
+
+const PurchasesContent: React.FC<PurchasesContentProps> = ({ beats }) => {
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+  const authModal = useAuthModal();
+
+  const onPlay = useOnPlay(beats);
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace('/');
+    }
+  }, [isLoading, user, router]);
+
+  if (beats.length === 0) {
+    return (
+      <div
+        className="
+          flex 
+          flex-col 
+          gap-y-2 
+          w-full px-6 
+          text-neutral-400
+        "
+      >
+        No purchased beats.
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-col gap-y-2 w-full p-6">
+      {beats.map((beat: any) => (
+        <div key={beat.id} className="flex items-center gap-x-4 w-full">
+          <div className="flex-1">
+            <MediaItem onClick={(id) => onPlay(id)} data={beat} />
+          </div>
+          <LikeButton beatId={beat.id} />
+          <DownloadButton beat={beat} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PurchasesContent;
