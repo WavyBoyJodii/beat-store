8ec2f25607c236199e78933f7308f4de494edb53
WavyBoyJodii <najeecasey@gmail.com>
2024-02-17 14:11:07 -0500

    remove hippo image from cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -122,16 +122,6 @@ const Cart = () => {
           </>
         ) : (
           <div className="flex h-full flex-col items-center justify-center space-y-1">
-            <div
-              aria-hidden="true"
-              className="relative mb-4 h-60 w-60 text-muted-foreground"
-            >
-              <Image
-                src="/hippo-empty-cart.png"
-                fill
-                alt="empty shopping cart hippo"
-              />
-            </div>
             <div className="text-xl font-semibold">Your cart is empty</div>
             <SheetTrigger asChild>
               <Link
